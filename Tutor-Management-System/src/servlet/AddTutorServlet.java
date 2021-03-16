1c056a1b8107d670d765c7aeaa29a5c417ef292d
Tyler <tfranks@ycp.edu>
2021-03-16 14:55:11 -0400

    Added AddTutorServlet and connected it via web xml

---
+++ Tutor-Management-System/src/servlet/AddTutorServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddTutorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		System.out.println("AddTutor Servlet: doGet");	
+		
+		// Call JSP to generate empty form
+		req.getRequestDispatcher("/_view/addTutor.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("AddNumbers Servlet: doPost");
+	}
+}
