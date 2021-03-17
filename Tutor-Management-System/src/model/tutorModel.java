cfc639b056bcbd3272789124e55729ddc5c6fb01
Ethan <esnyder16@ycp.edu>
2021-03-16 20:31:25 -0400

    Added Tutor Class and Test
    Created tutorModel and tutorModelTest files and added the class and the test

---
+++ Tutor-Management-System/src/model/tutorModel.java
@@ -0,0 +1,60 @@
+package model;
+
+/**
+ * The model for the tutor class of users.
+ * Contains tutors name, email, subject, payrate and tutorID
+ */
+
+public class tutorModel {
+    String name;
+    String email;
+    String subject;
+    double payRate;
+    int tutorID;
+
+    //Constructors
+    public tutorModel(){
+        name = "null";
+        email = "null";
+        subject = "null";
+        payRate = 0.0;
+        tutorID = 0;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+  
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+
+    public void setPayRate(double payRate){
+        this.payRate = payRate;
+    }
+    
+    public double getPayRate(){
+        return payRate;
+    }
+    
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+    
+    public String getSubject(){
+        return subject;
+    }
+    
+    //submitPayVoucher()
+    //getPayVoucher() //requires payvoucher class to be finished
+    //editPayVoucher()
+
+}
