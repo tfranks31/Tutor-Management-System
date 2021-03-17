0ae29494c28f7c58fdf0d598202736ec7b70222c
Theodread <zachary.roberts25@ycp.edu>
2021-03-17 00:07:17 -0400

    Added payVoucher view
    added PayVoucher jsp, css, and js

---
+++ Tutor-Management-System/war/_view/payVoucher.js
@@ -0,0 +1,33 @@
+var i;
+var tableSize = 10;
+
+document.addEventListener("DOMContentLoaded", function () {
+    for (i = 0; i < tableSize; i++) {
+        addRow();
+    }
+});
+
+var index = 0;
+function addRow() {
+    var table = document.getElementById("voucherInfo");
+    var row = table.insertRow(table.rows.length);
+    var cell1 = row.insertCell(0);
+    var t1 = document.createElement("input");
+        t1.id = "txtDate" + index;
+        cell1.appendChild(t1);
+    var cell2 = row.insertCell(1);
+    var t2 = document.createElement("input");
+        t2.id = "txtHours" + index;
+        cell2.appendChild(t2);
+    var cell3 = row.insertCell(2);
+    var t3 = document.createElement("input");
+        t3.id = "txtServicePerformed" + index;
+        cell3.appendChild(t3);
+    var cell4 = row.insertCell(3);
+    var t4 = document.createElement("input");
+        t4.id = "txtLocationPerformed" + index;
+        cell4.appendChild(t4);
+    index++;
+}
+
+
