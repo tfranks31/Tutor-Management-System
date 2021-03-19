d989f140eabac14d183e0afe1c1bfd4173f93000
Ethan <esnyder16@ycp.edu>
2021-03-18 21:40:43 -0400

    Added getter and setter for tutorID in tutor model

--- Tutor-Management-System/junit/model/TutorTest.java
+++ Tutor-Management-System/junit/model/TutorTest.java
@@ -4,39 +4,45 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TutorTest{
-    private Tutor model;
+public class TutorTest {
+	private Tutor model;
 
-    @Before
-    public void setUp(){
-        model = new Tutor();
+	@Before
+	public void setUp() {
+		model = new Tutor();
 
-    }
+	}
 
-    @Test
-    public void testSetGetName(){
-        model.setName("Ethan");
-        assertTrue(model.getName() == "Ethan");
-    }
+	@Test
+	public void testSetGetName() {
+		model.setName("Ethan");
+		assertTrue(model.getName() == "Ethan");
+	}
 
-    @Test
-    public void testSetGetEmail(){
-        model.setEmail("testemail@gmail.com");
-        assertTrue(model.getEmail() == "testemail@gmail.com");
-    }
+	@Test
+	public void testSetGetEmail() {
+		model.setEmail("testemail@gmail.com");
+		assertTrue(model.getEmail() == "testemail@gmail.com");
+	}
 
-    @Test
-    public void testSetGetPayRate(){
-        model.setPayRate(7.25);
-        assertTrue(model.getPayRate() == 7.25);
-    }
+	@Test
+	public void testSetGetPayRate() {
+		model.setPayRate(7.25);
+		assertTrue(model.getPayRate() == 7.25);
+	}
 
-    @Test
-    public void testSetSubject(){
-        model.setSubject("Computer Science");
-        assertTrue(model.getSubject() == "Computer Science");
-    }
-    //test submit payvoucher test
-    //get payvoucher test
-    //edit payvoucher test
+	@Test
+	public void testSetGetSubject() {
+		model.setSubject("Computer Science");
+		assertTrue(model.getSubject() == "Computer Science");
+	}
+
+	@Test
+	public void testSetGetTutorID() {
+		model.setTutorID(12344321);
+		assertTrue(model.getTutorID() == 12344321);
+	}
+	// test submit payvoucher test
+	// get payvoucher test
+	// edit payvoucher test
 }
