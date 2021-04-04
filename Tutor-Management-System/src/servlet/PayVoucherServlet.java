461eda0c225696cfa8dbb47f87d283bdcf047753
Tyler <tfranks@ycp.edu>
2021-04-04 13:04:02 -0400

    Pay Voucher loads entries, can add rows and carries rows over

--- Tutor-Management-System/src/servlet/PayVoucherServlet.java
+++ Tutor-Management-System/src/servlet/PayVoucherServlet.java
@@ -33,13 +33,12 @@ public class PayVoucherServlet extends HttpServlet{
 			ArrayList<Entry> entries = controller.getPayVoucherEntries(1);
 			
 			if (entries.size() < 10) {
-				
-				tableSize = 10;
+				tableSize = 10 - entries.size();
 			}
 			else {
-				
-				tableSize = entries.size();
+				tableSize = 0;
 			}
+			
 			req.setAttribute("tableSize", tableSize);
 			
 			req.setAttribute("entries", entries);
@@ -57,11 +56,33 @@ public class PayVoucherServlet extends HttpServlet{
 
 		// Go back to search
 		if (req.getParameter("addRow") != null) {	
-			if(tableSize < 15) {
-				tableSize += 1;
+			
+			controller = new PayVoucherController();
+			ArrayList<Entry> entries = new ArrayList<Entry>();
+			
+			String[] cells = req.getParameterValues("cell");
+			for (int i = 0; i < cells.length; i += 4) {
+				
+				if (!cells[i].equals("") && !cells[i + 1].equals("") &&
+					!cells[i + 2].equals("") && !cells[i + 3].equals("")) {
+					
+					Entry entry = new Entry();
+					entry.setDate(cells[i]);
+					entry.setHours(Double.parseDouble(cells[i + 1]));
+					entry.setServicePerformed(cells[i + 2]);
+					entry.setWherePerformed(cells[i + 3]);
+					entries.add(entry);
+				}
 			}
+			
+			tableSize = cells.length / 4 - entries.size() + 1;
 
+			if(tableSize + entries.size() > 15) {
+				tableSize = cells.length / 4 - entries.size();
+			}
 			req.setAttribute("tableSize", tableSize);
+			req.setAttribute("entries", entries);
+			
 			req.getRequestDispatcher("/_view/payVoucher.jsp").forward(req, resp);
 		}
 		
