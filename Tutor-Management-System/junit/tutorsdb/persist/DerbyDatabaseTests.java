f0b4bbc39d0968f3e8dd0beed967d96c0c29f45b
Ethan <esnyder16@ycp.edu>
2021-04-20 20:26:18 -0400

    testGetUserAccounts() and testGetTutors()

--- Tutor-Management-System/junit/tutorsdb/persist/DerbyDatabaseTests.java
+++ Tutor-Management-System/junit/tutorsdb/persist/DerbyDatabaseTests.java
@@ -46,4 +46,54 @@ public class DerbyDatabaseTests {
 		db.deleteTutor(dbTutor);
 		db.deleteUserAccount(dbAccount);
 	}
+	
+	@Test
+	public void testGetUserAccounts() {
+		
+		UserAccount newAccount = new UserAccount("user", "pass", -1, false);
+		Tutor newTutor = new Tutor("user pass", "user@user.use", "use", 1, -1, -1, "321", "123");
+		
+		db.addTutor(newAccount, newTutor);
+		
+		List<UserAccount> accountList = db.getUserAccounts();
+		List<Tutor> tutorList = db.getTutors();
+		
+		// Newly added objects should be the last object in the list
+		UserAccount dbAccount = accountList.get(accountList.size() - 1);
+		Tutor dbTutor = tutorList.get(tutorList.size() - 1);
+		
+		// Check that the objects were successfully entered
+		assertEquals(dbAccount.getUsername(), newAccount.getUsername());
+		assertEquals(dbTutor.getName(), newTutor.getName());
+		
+		// Remove test objects from tutorsdb
+		db.deleteTutor(dbTutor);
+		db.deleteUserAccount(dbAccount);
+		
+	}
+	
+	@Test
+	public void testGetTutor() {
+		
+		UserAccount newAccount = new UserAccount("user", "pass", -1, false);
+		Tutor newTutor = new Tutor("user pass", "user@user.use", "use", 1, -1, -1, "321", "123");
+		
+		db.addTutor(newAccount, newTutor);
+		
+		List<UserAccount> accountList = db.getUserAccounts();
+		List<Tutor> tutorList = db.getTutors();
+		
+		// Newly added objects should be the last object in the list
+		UserAccount dbAccount = accountList.get(accountList.size() - 1);
+		Tutor dbTutor = tutorList.get(tutorList.size() - 1);
+		
+		// Check that the objects were successfully entered
+		assertEquals(dbAccount.getUsername(), newAccount.getUsername());
+		assertEquals(dbTutor.getName(), newTutor.getName());
+		
+		// Remove test objects from tutorsdb
+		db.deleteTutor(dbTutor);
+		db.deleteUserAccount(dbAccount);
+		
+	}
 }
