3517843ded001322bedbfa0f2a4e5c37f2d6179f
Ethan <esnyder16@ycp.edu>
2021-04-20 20:14:36 -0400

    testAddtutor()

--- Tutor-Management-System/junit/tutorsdb/persist/DerbyDatabaseTests.java
+++ Tutor-Management-System/junit/tutorsdb/persist/DerbyDatabaseTests.java
@@ -1,6 +1,15 @@
 package tutorsdb.persist;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import model.Tutor;
+import model.UserAccount;
 
 public class DerbyDatabaseTests {
 
@@ -12,5 +21,29 @@ public class DerbyDatabaseTests {
 		// creating DB instance here
 		DatabaseProvider.setInstance(new DerbyDatabase());
 		db = DatabaseProvider.getInstance();				
+	}
+	
+	@Test
+	public void testAddTutor() {
+		
+		UserAccount newAccount = new UserAccount("user", "pass", -1, false);
+		Tutor newTutor = new Tutor("user pass", "user@user.use", "use", 1, -1, -1, "321", "123");
+		
+		db.addTutor(newAccount, newTutor);
+		
+		List<UserAccount> accountList = db.getUserAccounts();
+		List<Tutor> tutorList = db.getTutors();
+		
+		// Newly added objects should be the last object in the list
+		UserAccount dbAccount = accountList.get(accountList.size() - 1);
+		Tutor dbTutor = tutorList.get(tutorList.size() - 1);
+		
+		// Check that the objects were successfully entered
+		assertEquals(dbAccount.getUsername(), newAccount.getUsername());
+		assertEquals(dbTutor.getName(), newTutor.getName());
+		
+		// Remove test objects from tutorsdb
+		db.deleteTutor(dbTutor);
+		db.deleteUserAccount(dbAccount);
 	}
 }
