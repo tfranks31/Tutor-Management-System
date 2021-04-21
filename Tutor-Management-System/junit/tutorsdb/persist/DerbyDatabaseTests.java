b0bb8a06fa27757c8cbc3f5db3b4606fcc17bf1e
Michael <mmcleod2@ycp.edu>
2021-04-20 20:29:59 -0400

    Added delete tutor test

--- Tutor-Management-System/junit/tutorsdb/persist/DerbyDatabaseTests.java
+++ Tutor-Management-System/junit/tutorsdb/persist/DerbyDatabaseTests.java
@@ -1,6 +1,14 @@
 package tutorsdb.persist;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import model.Tutor;
+import model.UserAccount;
 
 public class DerbyDatabaseTests {
 
@@ -12,5 +20,33 @@ public class DerbyDatabaseTests {
 		DatabaseProvider.setInstance(new DerbyDatabase());
 		db = DatabaseProvider.getInstance();		
 		
+	}
+	
+	@Test
+	public void testDeleteTutor() {
+		UserAccount account = new UserAccount();
+		Tutor tutor = new Tutor();
+		int count = 0;
+		
+		account.setUsername("username");
+		account.setPassword("password");
+		tutor.setName("SuperRandomNameWeWillNeverUse");
+		tutor.setTutorID(123456789);
+		
+		db.addTutor(account, tutor);
+		
+		Tutor testTutor = db.getTutors().get(db.getTutors().size() - 1);
+		assertTrue(testTutor.getName().equals("SuperRandomNameWeWillNeverUse"));
+		
+		db.deleteTutor(testTutor);
+		db.deleteUserAccount(account);
+		
+		for (int i = 0; i < db.getTutors().size(); i++) {
+			if (db.getTutors().get(i).getName().equals("SuperRandomNameWeWillNeverUse")) {
+				count++;
+			}
+		}
+		
+		assertEquals(count, 0);
 	}
 }
